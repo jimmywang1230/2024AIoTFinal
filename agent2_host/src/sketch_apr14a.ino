787276c86aaa43c2b1381428363b16203c81f400
jimmywang1230 <jimmywang96172@gmail.com>
2024-05-28 21:22:14 +0800

    建立 class

--- agent2_host/src/sketch_apr14a.ino
+++ agent2_host/src/sketch_apr14a.ino
@@ -16,9 +16,25 @@ int i_lcd = 0; // Index for scrolling the LCD
 unsigned long previousMillis = 0; // will store last time LED was updated
 const long interval = 100; // interval at which to scroll the LCD
 
-int resistor1, resistor2, resistor3;
-int P1AgreeBtnSt, P1DisagreeBtnSt, P2AgreeBtnSt, P2DisagreeBtnSt, P3AgreeBtnSt, P3DisagreeBtnSt;
-int P1Showup, P2Showup, P3Showup;   // 可以出現在議場的立委0代表不行1可以
+class Participant {
+public:
+  String name;
+  int AgreeBtnSt;
+  int DisagreeBtnSt;
+  int Showup;
+  int resistor;
+
+  Participant() {
+    name = "";
+    AgreeBtnSt = 0;
+    DisagreeBtnSt = 0;
+    Showup = 0;
+    resistor = 0;
+  }
+};
+
+Participant P1, P2, P3;
+
 boolean inVoteMode; // 投票模式
 
 
@@ -27,9 +43,6 @@ String ShowupList = "SShowup List: ";
 String Alarm = "ALERT: ";
 String Alarm2 = "No show but vote";
 
-String P1 = "English Tsai";
-String P2 = "KP";
-String P3 = "Korea Han";
 
 void setup()
 {
@@ -41,9 +54,16 @@ void setup()
   pinMode(P2DisagreePin, INPUT);
   pinMode(P3AgreePin, INPUT);
   pinMode(P3DisagreePin, INPUT);
+
+  P1.name = "English Tsai";
+  P2.name = "KP";
+  P3.name = "Korea Fish";
+  P1.Showup=0, P2.Showup=0, P3.Showup=0;
+
+  
   Serial.begin(9600);
   btSerial.begin(9600);
-  P1Showup=0, P2Showup=0, P3Showup=0;
+  
   inVoteMode = false;
 
   lcd.begin(16, 2); // 初始化LCD
@@ -54,45 +74,43 @@ int prev=0;
 void loop()
 {
   unsigned long currentMillis = millis();
-  resistor1=analogRead(A0); 
-  resistor2=analogRead(A1); 
-  resistor3=analogRead(A2);
-  P1AgreeBtnSt= digitalRead(P1AgreePin);
-  P1DisagreeBtnSt= digitalRead(P1DisagreePin);
-  P2AgreeBtnSt= digitalRead(P2AgreePin);
-  P2DisagreeBtnSt= digitalRead(P2DisagreePin);
-  P3AgreeBtnSt= digitalRead(P3AgreePin);
-  P3DisagreeBtnSt= digitalRead(P3DisagreePin);
+  P1.resistor=analogRead(A0); 
+  P2.resistor=analogRead(A1); 
+  P3.resistor=analogRead(A2);
+  P1.AgreeBtnSt= digitalRead(P1AgreePin);
+  P1.DisagreeBtnSt= digitalRead(P1DisagreePin);
+  P2.AgreeBtnSt= digitalRead(P2AgreePin);
+  P2.DisagreeBtnSt= digitalRead(P2DisagreePin);
+  P3.AgreeBtnSt= digitalRead(P3AgreePin);
+  P3.DisagreeBtnSt= digitalRead(P3DisagreePin);
 
-  ShowupList = setShowupList(P1, P2, P3, P1AgreeBtnSt, P1DisagreeBtnSt, P2AgreeBtnSt, P2DisagreeBtnSt, P3AgreeBtnSt, P3DisagreeBtnSt);
+  ShowupList = setShowupList(P1, P2, P3);
   if (btSerial.available()){  //如果藍牙有傳資料過來
     int i = btSerial.read();  //把讀到的資料丟給i
     Serial.println(i);
     if (i == 1) {
       inVoteMode = true;
+      voteProcess(P1, P2, P3);
+
     } else if (i == 2) {
       inVoteMode = false;
     }
 
   }
   // 如果有人不應該來在位置上，而且還投票 蜂鳴器就會響
-  if (P1Showup == 0 && resistor1 < 600) {
-    if (P1AgreeBtnSt==1 || P1DisagreeBtnSt ==1) {
-      triggeredAlarm(P1);
-      lcd.begin(16, 2);
-
+  if (P1.Showup == 0 && P1.resistor < 600) {
+    if (P1.AgreeBtnSt==1 || P1.DisagreeBtnSt ==1) {
+      triggeredAlarm(P1.name);
     }
   }
-  if (P2Showup == 0 && resistor2 < 600) {
-    if (P2AgreeBtnSt==1 || P2DisagreeBtnSt ==1) {
-      triggeredAlarm(P2);
-      lcd.begin(16, 2);
+  if (P2.Showup == 0 && P2.resistor < 600) {
+    if (P2.AgreeBtnSt==1 || P2.DisagreeBtnSt ==1) {
+      triggeredAlarm(P2.name);
     }
   }
-  if (P3Showup == 0 && resistor3 < 600) {
-    if (P3AgreeBtnSt==1 || P3DisagreeBtnSt ==1) {
-      triggeredAlarm(P3);
-      lcd.begin(16, 2);
+  if (P3.Showup == 0 && P3.resistor < 600) {
+    if (P3.AgreeBtnSt==1 || P3.DisagreeBtnSt ==1) {
+      triggeredAlarm(P3.name);
     }
   }
   
@@ -117,31 +135,96 @@ void loop()
   delay(200);
 }
 
-String setShowupList(String P1, String P2, String P3, int P1AgreeBtnSt, int P1DisagreeBtnSt, int P2AgreeBtnSt, int P2DisagreeBtnSt, int P3AgreeBtnSt, int P3DisagreeBtnSt) {
+String voteProcess(Participant P1, Participant P2, Participant P3) {
+  String voteResult = "";
+  String agreeVote = "";
+  String disagreeVote = "";
+  int agreeCount = 0;
+  int disagreeCount = 0;
+  if (P1.Showup == 1 || P2.Showup == 1 || P3.Showup == 1) {
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Bill 1 Vote");
+    delay(1000);
 
-  if (P1Showup==0 && P1AgreeBtnSt == 1 && P1DisagreeBtnSt == 1){
-    P1Showup=1;
-  } else if (P2Showup==0 && P2AgreeBtnSt == 1 && P2DisagreeBtnSt ==1) {
-    P2Showup=1;
-  } else if (P3Showup==0 && P3AgreeBtnSt == 1 && P3DisagreeBtnSt == 1) {
-    P3Showup=1;
+    // Start the countdown
+    unsigned long startTime = millis();
+    int countdown = 10; // 10 seconds
+    while (countdown >= 0) {
+      // Check if a second has passed
+      if (millis() - startTime >= 1000) {
+        if (P1.AgreeBtnSt == 1 && P1.DisagreeBtnSt == 0) {
+          agreeVote += P1.name + " ";
+          lcd.setCursor(0, 1);
+
+        } else if (P1.AgreeBtnSt == 0 && P1.DisagreeBtnSt == 1) {
+          disagreeVote += P1.name + " ";
+          lcd.setCursor(0, 1);
+        } else if (P2.AgreeBtnSt ==1 && P2.DisagreeBtnSt ==0) {
+          agreeVote += P2.name + " ";
+          lcd.print(agreeVote);
+        } else if (P2.AgreeBtnSt ==0 && P2.DisagreeBtnSt ==1) {
+          disagreeVote += P2.name + " ";
+          lcd.print(disagreeVote);
+        } else if (P3.AgreeBtnSt ==1 && P3.DisagreeBtnSt ==0) {
+          agreeVote += P3.name + " ";
+          lcd.print(agreeVote);
+        } else if (P3.AgreeBtnSt ==0 && P3.DisagreeBtnSt ==1) {
+          disagreeVote += P3.name + " ";
+          lcd.print(disagreeVote);
+        }
+        lcd.setCursor(0, 2);
+        lcd.print("Agree Vote: ");
+        lcd.print(agreeVote);
+        lcd.setCursor(0, 1);
+        lcd.print("Time left: ");
+        lcd.print(countdown);
+        countdown--;
+        startTime = millis();
+      }
+    }
+    voteResult = agreeVote + " " + disagreeVote;
+    return voteResult;
+  } else {
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Not all show up");
+    lcd.setCursor(0,1);
+    lcd.print("Can't vote");
+    return "no show up";
   }
 
-  if (P1Showup==1) {
-    ShowupList += P1 + " ";
-    P1Showup=2;
-  } else if (P2Showup==1) {
-    ShowupList += P2 + " ";
-    P2Showup=2;
-  } else if (P3Showup==1) {
-    ShowupList += P3 + " ";
-    P3Showup=2;
+}
+
+
+// 顯示出席名單
+String setShowupList(Participant P1, Participant P2, Participant P3) {
+
+  if (P1.Showup==0 && P1.AgreeBtnSt == 1 && P1.DisagreeBtnSt == 1){
+    P1.Showup=1;
+  } else if (P2.Showup==0 && P2.AgreeBtnSt == 1 && P2.DisagreeBtnSt ==1) {
+    P2.Showup=1;
+  } else if (P3.Showup==0 && P3.AgreeBtnSt == 1 && P3.DisagreeBtnSt == 1) {
+    P3.Showup=1;
+  }
+
+  if (P1.Showup==1) {
+    ShowupList += P1.name + " ";
+    P1.Showup=2;
+  } else if (P2.Showup==1) {
+    ShowupList += P2.name + " ";
+    P2.Showup=2;
+  } else if (P3.Showup==1) {
+    ShowupList += P3.name + " ";
+    P3.Showup=2;
   } 
   delay(200);
   return ShowupList;
 }
 
+// 有人不在位卻投票時，觸發警報
 void triggeredAlarm(String Person) {
+  lcd.clear();
   digitalWrite(BuzzerPin,HIGH);
   delay(1000);
   digitalWrite(BuzzerPin,LOW);
@@ -158,4 +241,5 @@ void triggeredAlarm(String Person) {
     lcd.noDisplay();
     delay(500);
   }
+  lcd.begin(16, 2);
 }
